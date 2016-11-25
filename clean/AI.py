18520f1b8f56e24a661a7a2f330386c3bb3340a9
Llamaless <davisk14@uni.coventry.ac.uk>
2016-11-25 10:17:49 +0000

    Added retrospectively

--- clean/AI.py
+++ clean/AI.py
@@ -1,48 +1,58 @@
 import pygame
 import random
 
-WHITE = (255, 255, 255)
-
-clock = pygame.time.Clock()
+#Colours
+PURPLE = (255, 0, 255)
 
 class Com(pygame.sprite.Sprite):
     
-    def __init__(self, color, width, height):
-        #Loading all base components to make up AI 
-        super().__init__()
-        self.image = pygame.Surface([width, height])
-        self.image.fill(WHITE)
-        self.image.set_colorkey(WHITE)
-
-        pygame.draw.rect(self.image, color, [0, 0, width, height])
+    #AI sprite
+    def __init__(self, x, y):
+        super().__init__() 
+ 
+        #Model for AI sprite
+        self.image = pygame.Surface([20, 20])
+        self.image.fill(PURPLE)
         self.rect = self.image.get_rect()
-        #self.image = pygame.image.load('Com.png')
-        #self.rect = pygame.rect.Rect(55, 55) #, self.image.get_size())
+        self.rect.y = y
+        self.rect.x = x
+ 
+        #Ai sprite speed variable
+        self.xMoveSpeed = 0
+        self.yMoveSpeed = 0
 
-        timehascome = 0
-        move = 0
+    def playerSpeed(self, x, y):
+        self.xMoveSpeed += x
+        self.yMoveSpeed += y
+
+    def update(self):
+        self.rect.x += self.xMoveSpeed
+     ### Start of Mateusz contribution ### 
+        #Checks for collision with other sprites
+        check_collision = pygame.sprite.spritecollide(self, self.walls, False)
+        for collision in check_collision:
             
-        #def update(self):
-        dt = clock.tick(60)
-        last = self.rect.copy()
+            #front becomes direction of travel           
+            if self.xMoveSpeed > 0:
+                self.rect.right = collision.rect.left
 
-        #makes it so that ai only moves every 10 clock ticks
-        if(timehascome%10==0):
-            self.move = random.randint(1,5)
-        timehascome += 1
-                
-        #Ai moves 200 * clocktick in any given direction 
-        if self.move == 1:
-            self.rect.x -= 200 * dt
-        if self.move == 2:
-            self.rect.x += 200 * dt
-        if self.move == 3:
-            self.rect.y -= 200 * dt
-        if self.move == 4:
-            self.rect.y += 200 * dt
-                        
-            #during collisions ai self rectifies and tries to move in a different direction
-    """
-        for cell in pygame.sprite.spritecollide(self, game.walls, False):
-            self.rect = last
-            self.move = random.randint(1,5)"""
+            #front becomes direction of travel
+            else:
+                self.rect.left = collision.rect.right
+ 
+
+        self.rect.y += self.yMoveSpeed
+ 
+        #Checks for collision with other sprites
+        block_hit_list = pygame.sprite.spritecollide(self, self.walls, False)
+        for block in block_hit_list:
+ 
+            #front becomes direction of travel
+            if self.yMoveSpeed > 0:
+                self.rect.bottom = block.rect.top
+
+            #front becomes direction of travel
+            else:
+                self.rect.top = block.rect.bottom
+    ### End of Mateusz contribution ### 
+
