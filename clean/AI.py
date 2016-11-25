e46f88b713cc1d0858f23b0da905e49de6d44e67
Llamaless <davisk14@uni.coventry.ac.uk>
2016-11-25 10:02:14 +0000

    Add files via upload

--- clean/AI.py
+++ clean/AI.py
@@ -1,23 +1,36 @@
 import pygame
 import random
 
-class Com(pygame.sprite.Sprite):
-    dt = clock.tick(30)
-    def __init__(self, *groups):
-        #Loading all base components to make up AI 
-        super(Com, self).__init__(*groups)
-        self.image = pygame.image.load('Com.png')
-        self.rect = pygame.rect.Rect((55, 55), self.image.get_size())
-        self.timehascome = 0
-        self.move = 0
+WHITE = (255, 255, 255)
 
-    def update(self, dt, game):
-        last = self.rect.copy()
-        #makes it so that ai only moves every 10 clock ticks
-        if(self.timehascome%10==0):
-            self.move = random.randint(1,5)
-        self.timehascome += 1
+clock = pygame.time.Clock()
+
+class Com(pygame.sprite.Sprite):
+    
+    def __init__(self, color, width, height):
+        #Loading all base components to make up AI 
+        super().__init__()
+        self.image = pygame.Surface([width, height])
+        self.image.fill(WHITE)
+        self.image.set_colorkey(WHITE)
+
+        pygame.draw.rect(self.image, color, [0, 0, width, height])
+        self.rect = self.image.get_rect()
+        #self.image = pygame.image.load('Com.png')
+        #self.rect = pygame.rect.Rect(55, 55) #, self.image.get_size())
+
+        timehascome = 0
+        move = 0
             
+        #def update(self):
+        dt = clock.tick(60)
+        last = self.rect.copy()
+
+        #makes it so that ai only moves every 10 clock ticks
+        if(timehascome%10==0):
+            self.move = random.randint(1,5)
+        timehascome += 1
+                
         #Ai moves 200 * clocktick in any given direction 
         if self.move == 1:
             self.rect.x -= 200 * dt
@@ -27,8 +40,9 @@ class Com(pygame.sprite.Sprite):
             self.rect.y -= 200 * dt
         if self.move == 4:
             self.rect.y += 200 * dt
-                    
-        #during collisions ai self rectifies and tries to move in a different direction
+                        
+            #during collisions ai self rectifies and tries to move in a different direction
+    """
         for cell in pygame.sprite.spritecollide(self, game.walls, False):
             self.rect = last
-            self.move = random.randint(1,5)
+            self.move = random.randint(1,5)"""
